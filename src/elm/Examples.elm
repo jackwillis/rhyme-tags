690e52dfde924ec02cb8c0175b50cb80dd1fd2b9
Jack Willis <jack@attac.us>
2017-12-15 17:10:38 -0600

    Create longer example

--- src/elm/Examples.elm
+++ src/elm/Examples.elm
@@ -2,4 +2,25 @@ module Examples exposing (thingsYouCanDo)
 
 
 thingsYouCanDo =
-    "Things You Can Do\nby Deltron 3030\n\nso { tet : tah } os"
+    """
+
+Things You Can Do (Excerpt)
+by Deltron 3030
+
+Yo, Deltron { thunderforce: }, ain't no { other source: thunderforce }
+Of { sunlight: }, two { ton mic: sunlight }, leave you { tongue-tied: sunlight }
+Runnin' amok with { technology: } with no { apology: technology }
+Shout it out to { my colony: technology } with third { eye physiology: technology }
+Millennium past { apocalypse: } is { all I spit: apocalypse }
+Make you { swallow it: apocalypse }, your weak style, I'll { abolish it: apocalypse }
+With { nuclear rockets: } stay { glued to your optics: nuclear rockets } with sci-fi
+{ Unsettlin': } man and { metal blends: unsettlin' }
+Underground chillin' with the { mole man: } and his { whole fam: mole man }
+Inhibit { bacterial growth: }, { material growth: bacterial growth }
+Impenetrable, { incontestable: }, { indigestible: incontestable }
+{ Intelligence: }, never let a computer {tell me shit: intelligence}
+It's rapping { innovation: }, { penetrating: innovation }
+Artificial { life forms: life forms }, who { bite songs: life forms }
+I'mma { buy a Vex: }, { lye is next: buy a vex }, then I flip the { biotechs: buy a vex }
+
+""" |> String.trim
