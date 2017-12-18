b2632e8336cf21e505b24a679c5638f6a3f1ae07
Jack Willis <jack@attac.us>
2017-12-17 23:40:49 -0600

    Add examples selector

--- src/elm/Examples.elm
+++ src/elm/Examples.elm
@@ -1,8 +1,18 @@
-module Examples exposing (thingsYouCanDo)
+module Examples exposing (Example, thingsYouCanDo, fatCatsBiggaFish, harlemSweeties, allExamples)
+
+import Array exposing (Array)
 
 
+type alias Example =
+    { title : String, body : String }
+
+
+thingsYouCanDo : Example
 thingsYouCanDo =
-    """
+    Example
+        "Things You Can Do"
+    <|
+        String.trim """
 
 Things You Can Do (Excerpt)
 by Deltron 3030
@@ -23,4 +33,89 @@ It's rapping { innovation: }, { penetrating: innovation }
 Artificial { life forms: life forms }, who { bite songs: life forms }
 I'mma { buy a Vex: }, { lye is next: buy a vex }, then I flip the { biotechs: buy a vex }
 
-""" |> String.trim
+"""
+
+
+fatCatsBiggaFish : Example
+fatCatsBiggaFish =
+    Example
+        "Fat Cats, Bigga Fish"
+    <|
+        String.trim """
+
+Fat Cats, Bigga Fish (Excerpt)
+by The Coup
+
+It's almost 10 o'clock, see, I got a ball of lint for { property: }
+So I slip my beanie on { sloppily: property }
+And promenade out to take up a { collection: }
+I got game like I read the { directions: collection }
+I'm wishing that I had an auto{ mobile: }
+As I feel the cold wind { rush past: }
+But let me state that I'm a hustler { for real: mobile }
+So you know I got the stolen { bus pass: rush past }
+
+"""
+
+
+harlemSweeties : Example
+harlemSweeties =
+    Example
+        "Harlem Sweeties"
+    <|
+        String.trim """
+
+Harlem Sweeties
+by Langston Hughes
+
+Have you dug the { spill: }
+Of Sugar { Hill: spill }?
+Cast your gims
+On this sepia { thrill: spill }:
+Brown sugar lassie,
+Caramel { treat: },
+Honey-gold baby
+Sweet enough to { eat: treat }.
+Peach-skinned girlie,
+Coffee and { cream: },
+Chocolate darling
+Out of a { dream: cream }.
+Walnut tinted
+Or cocoa { brown: },
+Pomegranate-lipped
+Pride of the { town: brown }.
+Rich cream-colored
+To plum-tinted { black: },
+Feminine sweetness
+In Harlem’s no { lack: black }.
+Glow of the quince
+To blush of the { rose: rose }.
+Persimmon bronze
+To cinnamon { toes: toes }.
+Blackberry cordial,
+Virginia Dare { wine: }—
+All those sweet colors
+Flavor Harlem of { mine: wine }!
+Walnut or cocoa,
+Let me { repeat: treat }:
+Caramel, brown sugar,
+A chocolate { treat: }.
+Molasses taffy,
+Coffee and { cream: },
+Licorice, clove, cinnamon
+To a honey-brown { dream: cream }.
+Ginger, wine-gold,
+Persimmon, black{ berry: },
+All through the spectrum
+Harlem girls { vary: berry }—
+So if you want to know beauty’s
+Rainbow-sweet { thrill: spill },
+Stroll down luscious,
+Delicious, fine Sugar { Hill: spill }.
+
+"""
+
+
+allExamples : Array Example
+allExamples =
+    Array.fromList [ thingsYouCanDo, fatCatsBiggaFish, harlemSweeties ]
--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,11 +1,12 @@
 module Main exposing (main)
 
+import Array exposing (Array)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Markup exposing (ParseResult, parse)
 import Helpers exposing (displayResult)
-import Examples exposing (thingsYouCanDo)
+import Examples exposing (Example, thingsYouCanDo, allExamples)
 
 
 type alias Model =
@@ -14,11 +15,31 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model thingsYouCanDo (parse thingsYouCanDo), Cmd.none )
+    ( Model thingsYouCanDo.body (parse thingsYouCanDo.body), Cmd.none )
 
 
 type Msg
     = UpdateText String
+    | LoadExample String
+
+
+exampleOptions : List (Html a)
+exampleOptions =
+    let
+        optionFor n example =
+            option [ value (toString n) ] [ text example.title ]
+    in
+        Examples.allExamples
+            |> Array.indexedMap optionFor
+            |> Array.toList
+
+
+getExample : String -> Maybe Example
+getExample index =
+    index
+        |> String.toInt
+        |> Result.toMaybe
+        |> Maybe.andThen (\i -> Array.get i allExamples)
 
 
 view : Model -> Html Msg
@@ -35,22 +56,37 @@ view model =
                 , div [ class "output" ] [ displayResult model.result ]
                 ]
             , div []
-                [ h2 [] [ text "Settings" ]
+                [ h2 [] [ text "Documentation" ]
                 , p []
                     [ text "Read "
                     , a [ href "https://github.com/jackwillis/rhyme-tags" ] [ text "usage information" ]
                     , text " for rhyme-tags."
                     ]
+                , h2 [] [ text "Examples" ]
+                , select [ onInput LoadExample ] exampleOptions
                 ]
             ]
         ]
+
+
+updateText : String -> Model -> Model
+updateText text model =
+    { model | text = text, result = parse text }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         UpdateText text ->
-            ( { model | text = text, result = parse text }, Cmd.none )
+            ( model |> updateText text, Cmd.none )
+
+        LoadExample num ->
+            case (getExample num) of
+                Just example ->
+                    ( model |> updateText example.body, Cmd.none )
+
+                Nothing ->
+                    ( { model | text = "No such example #" ++ num, result = parse "" }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
