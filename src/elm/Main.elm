dcc3bedf193c0d61d8f1534004b74e6a6caceec5
Jack Willis <jack@attac.us>
2017-12-31 20:55:30 -0600

    Fix typos

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -138,7 +138,7 @@ aboutBody =
     div []
         [ p []
             [ text "rhyme-tags version "
-            , a [ href "https://github.com/jackwillis/rhyme-tags/releases/tag/v0.1.5" ]
+            , a [ href "https://github.com/jackwillis/rhyme-tags/releases/tag/v0.1.6" ]
                 [ text "0.1.6" ]
             , text "."
             ]
